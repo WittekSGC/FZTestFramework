68b1f3ca32c513b30d659da927feb91ade9a3b22
Wittek Zholudov <zoludovv@gmail.com>
2020-03-03 20:59:30 +0300

    lastUpdate

---
+++ TestProject/BasePage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    public class BasePage
+    {
+        private DriverClass driver = new DriverClass(@"https:\\google.com", "Chrome");
+        public IWebDriver _driver;
+
+        public BasePage()
+        {
+            _driver = driver.GetDriver();
+        }
+
+        public IWebElement FindElement(By locator)
+        {
+            return _driver.FindElement(locator);
+        }
+
+        public void CloseDriver()
+        {
+            _driver.Quit();
+        }
+    }
+}
---
+++ TestProject/DriverClass.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestProject
+{
+    public class DriverClass
+    {
+        private IWebDriver driver;
+        public DriverClass(string path, string mode)
+        {
+            switch (mode)
+            {
+                case "Chrome":
+                    {
+                        driver = new ChromeDriver() { Url = path};
+                        //driver.Url = path;
+                        break;
+                    }
+                case "Firefox":
+                    {
+                        driver = new FirefoxDriver();
+                        driver.Url = path;
+                        break;
+                    }
+                default:
+                    {
+                        throw new Exception("unknown mode driver");
+                    }
+            }
+        }
+
+        public IWebDriver GetDriver()
+        {
+            return driver;
+        }
+
+
+    }
+}
